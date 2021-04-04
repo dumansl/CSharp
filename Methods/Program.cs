b4e4dbbb9fc1557a43e3ef30c97df38cfb56c616
dumansl <36080546+dumansl@users.noreply.github.com>
2021-04-04 16:44:54 +0300

    Add files via upload

---
+++ Methods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var result3 = Add(50);
+            //var result2 = Add(20,30);
+            //Console.WriteLine(result2);
+            //Console.WriteLine(result3);
+            //Console.ReadLine();
+
+            //int number1 = 10;
+            //int number2 = 20;
+            //var result = Add2(ref number1, number2); //ref alternatifi out fark ref öncesi değer atama zorunlu.
+            
+            //Console.WriteLine(Multiply(2,4));
+            //Console.WriteLine(Multiply(2, 4,5));
+
+            Console.WriteLine(Add3(1,2,3,4,5));
+            Console.ReadLine();
+        }
+
+        static int Add(int number1, int number2=10)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+
+        static int Add2(ref int number1, int number2)
+        {
+            number1 = 20;
+            return number1 + number2;
+
+        }
+
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
+
+        static int Add3(int number1,params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+    }
+}
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            //Console.WriteLine("Hello Word!");
+            bool condition = true; // boolean value is true/false
+            char character = 'A'; 
+            int number1 = 2147483647; // int max value
+            long number2 = 9223372036854775807; // long max value
+            int number3 = -2147483648;  // int min value
+            long number4 = -9223372036854775808; // long min value
+            short number5 = 32767; // short max value
+            short number6 = -32768; // short min value
+            byte number7 = 255; // byte range 0-255
+            double number8 = 10.4;
+            decimal number9 = 10.4m;
+            var number10 = 181;
+            number10 = 'A';
+
+            Console.WriteLine("Interger max value is {0}", number1);
+            Console.WriteLine("Interger min value is {0}", number3);
+            Console.WriteLine("Long max value is {0}", number2);
+            Console.WriteLine("Long min value is {0}", number4);
+            Console.WriteLine("Short max value is {0}", number5);
+            Console.WriteLine("Short min value is {0}", number6);
+            Console.WriteLine("Byte max value is {0}", number7);
+            Console.WriteLine("Character is : {0}", (int)character);
+            Console.WriteLine("Double number is {0}", number8);
+            Console.WriteLine("Decimal number is {0}", number9);
+            Console.WriteLine("Number10 is {0}", number10);
+            Console.WriteLine((int)Days.carsamba);
+            Console.ReadLine();
+        }
+    }
+    enum Days
+    {
+        pazartesi,sali,carsamba,persembe,cuma,cumartesi,pazar
+    }
+}
